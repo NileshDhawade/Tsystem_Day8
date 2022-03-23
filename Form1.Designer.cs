ba921bcff0a3627d392bffabb1bf7498e1a33713
Nilesh Dhawade <dhawade.nilesh007@gmail.com>
2022-03-23 21:10:02 +0530

    ADO.NET_Form_Example

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lName = new System.Windows.Forms.Label();
+            this.txtDesignation = new System.Windows.Forms.TextBox();
+            this.lDesignation = new System.Windows.Forms.Label();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.lSalary = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.lId = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(924, 102);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 36);
+            this.btnSearch.TabIndex = 25;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(924, 155);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 34);
+            this.btnDelete.TabIndex = 24;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(529, 433);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(116, 47);
+            this.btnAddNew.TabIndex = 23;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(651, 433);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(106, 47);
+            this.btnSave.TabIndex = 22;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(763, 433);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(102, 47);
+            this.btnUpdate.TabIndex = 21;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(614, 176);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(189, 26);
+            this.txtName.TabIndex = 20;
+            // 
+            // lName
+            // 
+            this.lName.AutoSize = true;
+            this.lName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lName.Location = new System.Drawing.Point(496, 179);
+            this.lName.Name = "lName";
+            this.lName.Size = new System.Drawing.Size(64, 25);
+            this.lName.TabIndex = 19;
+            this.lName.Text = "Name";
+            // 
+            // txtDesignation
+            // 
+            this.txtDesignation.Location = new System.Drawing.Point(614, 250);
+            this.txtDesignation.Name = "txtDesignation";
+            this.txtDesignation.Size = new System.Drawing.Size(189, 26);
+            this.txtDesignation.TabIndex = 18;
+            // 
+            // lDesignation
+            // 
+            this.lDesignation.AutoSize = true;
+            this.lDesignation.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lDesignation.Location = new System.Drawing.Point(496, 256);
+            this.lDesignation.Name = "lDesignation";
+            this.lDesignation.Size = new System.Drawing.Size(115, 25);
+            this.lDesignation.TabIndex = 17;
+            this.lDesignation.Text = "Designation";
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(614, 325);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(189, 26);
+            this.txtSalary.TabIndex = 16;
+            // 
+            // lSalary
+            // 
+            this.lSalary.AutoSize = true;
+            this.lSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lSalary.Location = new System.Drawing.Point(496, 325);
+            this.lSalary.Name = "lSalary";
+            this.lSalary.Size = new System.Drawing.Size(68, 25);
+            this.lSalary.TabIndex = 15;
+            this.lSalary.Text = "Salary";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(614, 102);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(189, 26);
+            this.txtId.TabIndex = 14;
+            // 
+            // lId
+            // 
+            this.lId.AutoSize = true;
+            this.lId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lId.Location = new System.Drawing.Point(496, 105);
+            this.lId.Name = "lId";
+            this.lId.Size = new System.Drawing.Size(28, 25);
+            this.lId.TabIndex = 13;
+            this.lId.Text = "Id";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1403, 673);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lName);
+            this.Controls.Add(this.txtDesignation);
+            this.Controls.Add(this.lDesignation);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.lSalary);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lId);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lName;
+        private System.Windows.Forms.TextBox txtDesignation;
+        private System.Windows.Forms.Label lDesignation;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Label lSalary;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lId;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,186 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace Tsystem_ADO.NET_Demo
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
+            SqlConnection con;
+            SqlCommand cmd;
+            SqlDataReader dr;
+
+            public Form1()
+            {
+                InitializeComponent();
+                con = new SqlConnection(@"Server=NILESH\SQLEXPRESS;Database=T_System_PracticeDB;Integrated Security=True");
+
+            }
+            public void ClearData()
+            {
+                txtId.Clear();
+                txtName.Clear();
+                txtDesignation.Clear();
+                txtSalary.Clear();
+            }
+
+            private void btnSave_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    //to pass values into the insert query we will use paeameters(parametric query)
+                    string qry = "insert into Employee values(@id,@name,@designation,@salary)";
+                    cmd = new SqlCommand(qry, con);
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtId.Text));
+                    cmd.Parameters.AddWithValue("@name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@designation", txtDesignation.Text);
+                    cmd.Parameters.AddWithValue("@salary", Convert.ToDouble(txtSalary.Text));
+                    int result = cmd.ExecuteNonQuery();
+                    if (result == 1)
+                    {
+                        MessageBox.Show("successfully saved the record");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+
+            private void btnSearch_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    string qry = "select * from Employee where Id=@id";
+                    cmd = new SqlCommand(qry, con);
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtId.Text));
+                    dr = cmd.ExecuteReader();
+                    //first will check that record is present
+                    if (dr.HasRows)
+                    {
+                        //read the record from dr object
+                        if (dr.Read())
+                        {
+                            txtName.Text = dr["Name"].ToString();
+                            txtDesignation.Text = dr["Designation"].ToString();
+                            txtSalary.Text = dr["Salary"].ToString();
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+
+            private void btnUpdate_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    //to pass values into the insert query we will use paeameters(parametric query)
+                    string qry = "update Employee set Name=@name,Designation=@designation,Salary=@salary where Id=@id";
+                    cmd = new SqlCommand(qry, con);
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtId.Text));
+                    cmd.Parameters.AddWithValue("@name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@designation", txtDesignation.Text);
+                    cmd.Parameters.AddWithValue("@salary", Convert.ToDouble(txtSalary.Text));
+                    int result = cmd.ExecuteNonQuery();
+                    if (result == 1)
+                    {
+                        MessageBox.Show("successfully updated the record");
+                    }
+                    ClearData();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+
+            private void btnDelete_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    //to pass values into the insert query we will use paeameters(parametric query)
+                    string qry = "delete from Employee where Id=@id";
+                    cmd = new SqlCommand(qry, con);
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtId.Text));
+
+                    int result = cmd.ExecuteNonQuery();
+                    if (result == 1)
+                    {
+                        MessageBox.Show("successfully deleted the record");
+                    }
+                    ClearData();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+                }
+
+            }
+
+            private void btnAddNew_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    string qry = "select MAX(Id) from Employee";
+                    cmd = new SqlCommand(qry, con);
+                    con.Open();
+                    object obj = cmd.ExecuteScalar();
+                    //DBNull is used to check existance og value in the obj
+                    if (obj == DBNull.Value)
+                    {
+                        txtId.Text = "1";
+                    }
+                    else
+                    {
+                        int id = Convert.ToInt32(obj);//will get the MAX id from Employee
+                        id++;
+                        txtId.Text = id.ToString();
+                    }
+                    txtId.Enabled = false; ;
+                    ClearData();
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+
+                }
+            }
         }
+
     }
-}
+
