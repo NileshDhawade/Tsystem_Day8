1131ae656bad36f7ba0d208045da701945234cca
Nilesh Dhawade <dhawade.nilesh007@gmail.com>
2022-03-23 21:15:28 +0530

    ADO.NET_Example2

---
+++ Form2.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Tsystem_ADO.NET_Demo
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lName = new System.Windows.Forms.Label();
+            this.txtDesignation = new System.Windows.Forms.TextBox();
+            this.lDesignation = new System.Windows.Forms.Label();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.lSalary = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.lId = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(810, 94);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 36);
+            this.btnSearch.TabIndex = 38;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(810, 147);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 34);
+            this.btnDelete.TabIndex = 37;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(415, 425);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(116, 47);
+            this.btnAddNew.TabIndex = 36;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(537, 425);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(106, 47);
+            this.btnSave.TabIndex = 35;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(649, 425);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(102, 47);
+            this.btnUpdate.TabIndex = 34;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(500, 168);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(189, 26);
+            this.txtName.TabIndex = 33;
+            // 
+            // lName
+            // 
+            this.lName.AutoSize = true;
+            this.lName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lName.Location = new System.Drawing.Point(382, 171);
+            this.lName.Name = "lName";
+            this.lName.Size = new System.Drawing.Size(64, 25);
+            this.lName.TabIndex = 32;
+            this.lName.Text = "Name";
+            // 
+            // txtDesignation
+            // 
+            this.txtDesignation.Location = new System.Drawing.Point(500, 242);
+            this.txtDesignation.Name = "txtDesignation";
+            this.txtDesignation.Size = new System.Drawing.Size(189, 26);
+            this.txtDesignation.TabIndex = 31;
+            // 
+            // lDesignation
+            // 
+            this.lDesignation.AutoSize = true;
+            this.lDesignation.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lDesignation.Location = new System.Drawing.Point(382, 248);
+            this.lDesignation.Name = "lDesignation";
+            this.lDesignation.Size = new System.Drawing.Size(115, 25);
+            this.lDesignation.TabIndex = 30;
+            this.lDesignation.Text = "Designation";
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(500, 317);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(189, 26);
+            this.txtSalary.TabIndex = 29;
+            // 
+            // lSalary
+            // 
+            this.lSalary.AutoSize = true;
+            this.lSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lSalary.Location = new System.Drawing.Point(382, 317);
+            this.lSalary.Name = "lSalary";
+            this.lSalary.Size = new System.Drawing.Size(68, 25);
+            this.lSalary.TabIndex = 28;
+            this.lSalary.Text = "Salary";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(500, 94);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(189, 26);
+            this.txtId.TabIndex = 27;
+            // 
+            // lId
+            // 
+            this.lId.AutoSize = true;
+            this.lId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lId.Location = new System.Drawing.Point(382, 97);
+            this.lId.Name = "lId";
+            this.lId.Size = new System.Drawing.Size(28, 25);
+            this.lId.TabIndex = 26;
+            this.lId.Text = "Id";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1164, 617);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lName);
+            this.Controls.Add(this.txtDesignation);
+            this.Controls.Add(this.lDesignation);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.lSalary);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lId);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lName;
+        private System.Windows.Forms.TextBox txtDesignation;
+        private System.Windows.Forms.Label lDesignation;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Label lSalary;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lId;
+    }
+}
